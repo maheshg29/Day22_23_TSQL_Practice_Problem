c580260e9809cc13702a7f6f233b869148f167bb
mahesh <gangurdemahesh9@gmail.com>
2023-04-23 23:06:14 +0530

    [mahesh]add Welcome greeting message

---
+++ Day22-23-TSQL_Practice_Problem/CURDOperation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_23_TSQL_Practice_Problem
+{
+    internal class CURDOperation
+    {
+    }
+}
---
+++ Day22-23-TSQL_Practice_Problem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_23_TSQL_Practice_Problem
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 22 and day 23 TSQL Practice Problem");
+        }
+    }
+}
